c873eabaefc7b7473f8d6bb519257db648c027cf
Sergio Olivares <sergio.olivares@doofinder.com>
2021-11-19 17:28:08 +0100

    Final UI done. Fonts included. Design for the info panel and the fleet panel finished. Creation of the fleet depending on the constant done. Now what's left is the configuration of this UI depending on the parameters of the state of the game (socket).

--- battleship/lib/battleship_web/live/setting_live.ex
+++ battleship/lib/battleship_web/live/setting_live.ex
@@ -2,7 +2,7 @@ defmodule BattleshipWeb.SettingLive do
   use BattleshipWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, %{board_size: 10, available_boats: [{5,1}, {4,1}, {3, 2}, {2, 1}]})}
+    {:ok, assign(socket, %{board_size: 10, available_boats: [5, 4, 3, 3, 2]})}
   end
 
 end
