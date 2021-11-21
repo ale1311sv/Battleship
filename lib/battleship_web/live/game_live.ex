965ee42f2d99d2e48dfe1550933c7d03416b4f6c
Sergio Olivares <sergio.olivares@doofinder.com>
2021-11-21 19:07:22 +0100

    Template and source code for live pages changed: now the heex files are separated in a different folder, in templates/page, while the ex files remain in live/. This allows to have heex templates for repeated heex components, like the fleet. Some new events ("boat_selected" and "cell_selected") have been also added into the setting template page.

--- lib/battleship_web/live/game_live.ex
+++ lib/battleship_web/live/game_live.ex
@@ -5,4 +5,8 @@ defmodule BattleshipWeb.GameLive do
     {:ok, assign(socket, %{board_size: 10, available_boats: [5, 4, 3, 3, 2]})}
   end
 
+  def render(assigns) do
+    BattleshipWeb.PageView.render("game_live.html", assigns)
+  end
+
 end
--- lib/battleship_web/live/setting_live.ex
+++ lib/battleship_web/live/setting_live.ex
@@ -5,4 +5,13 @@ defmodule BattleshipWeb.SettingLive do
     {:ok, assign(socket, %{board_size: 10, available_boats: [5, 4, 3, 3, 2]})}
   end
 
+  def handle_event("boat_selected", %{"length" => length}, socket) do
+    {:noreply, assign(socket, :available_boats, List.delete(socket.assigns.available_boats, String.to_integer(length)))}
+  end
+
+  def render(assigns) do
+    BattleshipWeb.PageView.render("setting_live.html", assigns)
+  end
+
+
 end
