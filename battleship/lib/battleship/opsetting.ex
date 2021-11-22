6a898963c4b1d37af11a119445e4ecb541e96e1c
nurietzche <nuria.gallego@doofinder.com>
2021-11-22 17:12:51 +0100

    Now setting operations for GAME are ready.

--- battleship/lib/battleship/opsetting.ex
+++ battleship/lib/battleship/opsetting.ex
@@ -1,4 +1,5 @@
 defmodule Battleship.Opsetting do
+  
   @doc """
     This function checks the validity of intended boat location in the player's game table
   """
@@ -16,7 +17,7 @@ defmodule Battleship.Opsetting do
       false
     end
   end
-  
+
   def is_position_valid?(boat, list_boats = [ set_boat | tail ], available_boats) do
     if is_boat_available?(boat, list_boats, available_boats) do
       is_position_valid?(boat, [set_boat], available_boats) && is_position_valid?(boat, tail, available_boats)
@@ -26,12 +27,22 @@ defmodule Battleship.Opsetting do
   end
 
 
+  @doc """
+   Function to check if player set all boats already
+  """
+  @spec all_boats_set?([boat], [integer]) :: boolean
+  
+  def all_boats_set?(boats, available_boats), do: boats_left_player(boats,available_boats) == []
+
+
+
   #SUPPORT FUNCTIONS 
 
   @doc """
   Function to check if cells of a boat are inside grid
   """
   @spec is_boat_on_grid?(boat) :: boolean
+  
   def is_boat_on_grid?(boat) do
     for i <- 0 .. length(boat) - 1 do
       Enum.at( boat, i)
@@ -42,10 +53,12 @@ defmodule Battleship.Opsetting do
     |> Enum.filter(&( &1 == false )) == []
   end
 
+
   @doc """
     Function to consider the illegal cells to locate boats given a 
     """
   @spec illegal_cells(boat) :: [cell]
+  
   def illegal_cells(set_boat) do
     illegal = set_boat
     for {x,y} <- set_boat do
@@ -56,13 +69,27 @@ defmodule Battleship.Opsetting do
     |> Enum.sort()
   end
 
+
+  @doc """
+  Function that returns the available boats for player to locate by their length
+  """
+  @spec boats_left_player([boat], list) :: boolean
+  
+  def boats_left_player(list_boats, available_boats) do
+    available_boats -- Enum.map(list_boats, &(length(&1)))
+  end
+
+
   @doc """
   Function to check if boat is available
   """
   @spec is_boat_available?(boat, [boat], list) :: boolean
+  
   def is_boat_available?(boat, list_boats, available_boats) do
-    available_boats -- Enum.map(list_boats, &(length(&1)))
+    boats_left_player(list_boats, available_boats)
     |> Enum.sort()
-    |> Enum.member?(length(boat))
+    |> Enum.member?( length( boat) )
   end
+
+
 end
