719257ba311f66b9392465ca0336c77930da6cc2
Pabloj20 <pablo.rubio@doofinder.com>
2021-11-22 13:09:12 +0100

    insert_boat functions

--- battleship/lib/battleship/game.ex
+++ battleship/lib/battleship/game.ex
@@ -24,10 +24,61 @@ defmodule Battleship.Game do
       state
       |> put_in([:player2, :pid], pid)
       |> Map.put(:mode, :setting)
+
     {:ok, state}
   end
 
   def join(_, _) do
     {:error, "Game is full"}
+  end
+
+  def insert_boat(boat, %{player1: %{boats: boats_list}} = state, pid)
+      when state.player1.pid == pid do
+    # if is_position_valid?(boat, boats_list) do
+    #   state =
+    #     state
+    #     |> put_in([:player1, :boats], state.player1.boats ++ boat)
+
+    #   if all_boats_in_player?(get_in(state, [:player1, :boats])) do
+    #     if all_boats_in?(get_in(state, [:player1, :boats]), get_in(state, [:player2, :boats])) do
+    #       state =
+    #         state
+    #         |> Map.put(:mode, :p1)
+
+    #       {:ready, state}
+    #     else
+    #       {:full, state}
+    #     end
+    #   else
+    #     {:ok, state}
+    #   end
+    # else
+    #   {:error, "Position no valid"}
+    # end
+  end
+
+  def insert_boat(boat, %{player2: %{boats: boats_list}} = state, pid)
+      when state.player2.pid == pid do
+    # if is_position_valid?(boat, boats_list) do
+    #   state =
+    #     state
+    #     |> put_in([:player1, :boats], state.player2.boats ++ boat)
+
+    #   if all_boats_in_player?(get_in(state, [:player2, :boats])) do
+    #     if all_boats_in?(get_in(state, [:player1, :boats]), get_in(state, [:player2, :boats])) do
+    #       state =
+    #         state
+    #         |> Map.put(:mode, :p1)
+
+    #       {:ready, state}
+    #     else
+    #       {:full, state}
+    #     end
+    #   else
+    #     {:ok, state}
+    #   end
+    # else
+    #   {:error, "Position no valid"}
+    # end
   end
 end
