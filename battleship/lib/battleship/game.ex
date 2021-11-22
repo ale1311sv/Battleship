18940c5ef3b77df47a56a2d7e847c886e8842d68
Rodrigo Molina <rodrigomolina@doofinder.com>
2021-11-22 13:02:32 +0100

    First aproach to the api of de game in de playing mode

--- battleship/lib/battleship/game.ex
+++ battleship/lib/battleship/game.ex
@@ -1,59 +1,37 @@
-defmodule Game do
-  use GenServer
+defmodule Battleship.Game do
+	# JOIN MODE
 
-  #callbacks
 
-  def init(state) do
-    {:ok, state}
-  end
+	# SETTING MODE
 
-@doc """
-	It checks the position of the boat.
-		If it`s valid(true)
-		-> set new_state and check if all the boats of the player are set
-			If true
-				->check if all the boats(both players) are set
-					If true
-						-> {:reply, {:p1, change_state}, change_state}
-					else
-						-> {:reply, {:full, new_state}, new_state}
-			else
-				-> {:reply, {:setting, new_state}, new_state}
+
+	# PLAYING MODE
+	@spec make_shot(tuple(), map()) :: {atom(), term()}
+	def make_shot(shot, %{mode: :initial}) do
+		{:error, "You cannot shot in initial mode"}
+	end
+
+	def make_shot(shot, %{mode: :setting}) do
+		{:error, "You cannot shot in setting mode"}
+	end
+
+	def make_shot(shot, %{mode: :p1} = state) do
+		if shot_valid?(shot, state.player1.shots) do
+			state = put_in(state, [:player1, :shots], [shot] ++ state.player1.shots)
+			conseq = conseq_shots(state.player1.shots, state.player2.boats)
+			{conseq, state}
 		else
-			-> {:reply, {:no_valid, old_state}, old_state}
-"""
-	def handle_call({:insert_boat, list}, _from, {_, :setting} = old_state) do
-		#if position_is_valid? do
-			#new_state =	#insert_boat(old_state,list)
-			#if all_boats_in_player? do
-				#if all_boats_in? do
-					#{map, _} = new_state
-					#change_state = {map, :p1}
-					#{:reply, {:p1, change_state}, change_state}
-				#else
-					#{:reply, {:full, new_state}, new_state}
-				#end
-			#else
-				#{:reply, {:setting, new_state}, new_state}
-			#end
-		# else
-			#{:reply, {:no_valid, old_state}, old_state}
-		# end
+			{:error, state}
+		end
 	end
 
-
-	@doc """
-	When all the boats(both players) are ready, it replies to the player who set first the boats that the game is ready
-	"""
-	def handle_call(:full_all?, _from, {_, atom} =state) do
-		# if atom == :setting do
-		# 	{:reply, false, state}
-		# else
-		# 	{:reply, true, state}
-		# end
+	def make_shot(shot, %{mode: :p2} = state) do
+		if shot_valid?(shot, state.player2.shots) do
+			state = put_in(state, [:player2, :shots], [shot] ++ state.player1.shots)
+			conseq = conseq_shots(state.player1.shots, state.player1.boats)
+			{conseq, state}
+		else
+			{:error, state}
+		end
 	end
-
-
-
-
 end
